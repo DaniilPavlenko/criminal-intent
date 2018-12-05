1705356eb0d476db8e971023e0b0811069480bfe
Daniil <pavlenko.develop@gmail.com>
2018-12-05 13:00:27 +0300

    Add Database create

--- app/src/main/java/ru/dpav/criminalintent/CrimeLab.java
+++ app/src/main/java/ru/dpav/criminalintent/CrimeLab.java
@@ -1,6 +1,7 @@
 package ru.dpav.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -8,26 +9,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import ru.dpav.criminalintent.database.CrimeBaseHelper;
+
 class CrimeLab {
     private static CrimeLab sCrimeLab;
     private Map<UUID, Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    private CrimeLab(Context context) {
+        mContext = context;
+        mDatabase = new CrimeBaseHelper(context).getWritableDatabase();
+        mCrimes = new LinkedHashMap<>();
+//        for (int i = 0; i < 2; i++) {
+//            Crime crime = new Crime();
+//            crime.setTitle("Crime #" + i);
+//            crime.setSolved(i % 2 == 0);
+//            crime.setRequiresPolice(i % 5 == 0);
+//            mCrimes.put(crime.getId(), crime);
+//        }
+    }
 
     static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
-            sCrimeLab = new CrimeLab();
+            sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
-    }
-
-    private CrimeLab() {
-        mCrimes = new LinkedHashMap<>();
-        for (int i = 0; i < 2; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            crime.setRequiresPolice(i % 5 == 0);
-            mCrimes.put(crime.getId(), crime);
-        }
     }
 
     List<Crime> getCrimes() {
